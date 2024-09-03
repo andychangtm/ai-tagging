3e80e7c42a3faf9a70a9766157ceeec1cf17f593
andychangdev <andychang.qld@gmail.com>
2024-09-03 23:57:36 +1000

    feat: Called OpenAI API to describe image file on local device

--- task1.py
+++ task1.py
@@ -1,34 +1,55 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
+import base64
+import requests
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
+IMAGE_PATH = "beach.jpg"
 API_KEY = os.getenv('api_key')
+API_URL = "https://api.openai.com/v1/chat/completions"
+MODEL = "gpt-4o-mini"
+PROMPT = "What's in this image?"
 
-client = OpenAI(
-    api_key= API_KEY
-)
 
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "What’s in this image?"},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://ihe-delft-ihe-website-production.s3.eu-central-1.amazonaws.com/s3fs-public/styles/530x530/public/2022-11/Sea%20level%20rise%20Vietnam.jpg",
-          },
-        },
-      ],
+def encode_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def get_response_from_api(encoded_image):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
     }
-  ],
-  max_tokens=300,
-)
+    payload = {
+        "model": MODEL,
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": PROMPT
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encoded_image}"
+                        },
+                    },
+                ],
+            }
+        ],
+        "max_tokens": 300,
+    }
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
-print(response.choices[0])
-print(response.usage)
+
+if __name__ == "__main__":
+    encoded_image = encode_image_to_base64(IMAGE_PATH)
+    response_dict = get_response_from_api(encoded_image)
+    message_content = response_dict["choices"][0]["message"]["content"]
+    print("Response Message:")
+    print(message_content)
