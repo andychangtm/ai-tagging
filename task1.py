2ce8f5e15d400c6d9e0b7878252e0866aa5343f5
andychangdev <andychang.qld@gmail.com>
2024-09-03 23:52:39 +1000

    feat: Called OpenAI API to describe an online image

--- task1.py
+++ task1.py
@@ -10,11 +10,25 @@ API_KEY = os.getenv('api_key')
 client = OpenAI(
     api_key= API_KEY
 )
-prompt = "Give me a most popular hotel in Brisbane"
-chat_completion = client.chat.completions.create(
-    messages=[
-        {"role": "user","content": prompt}
-    ],
-    model="gpt-4o-mini",
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://ihe-delft-ihe-website-production.s3.eu-central-1.amazonaws.com/s3fs-public/styles/530x530/public/2022-11/Sea%20level%20rise%20Vietnam.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
 )
-print("".join([choice.message.content for choice in chat_completion.choices]))
+
+print(response.choices[0])
+print(response.usage)
